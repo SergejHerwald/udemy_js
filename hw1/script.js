522f16b3b80549308d9e0185f985656d4b7d8d57
s.herwald <1825666@stud.hs-mannheim.de>
2020-03-22 14:27:50 +0100

    homework upgraded

--- hw1/script.js
+++ hw1/script.js
@@ -2,20 +2,39 @@ var money = +prompt("Your budget for a month?");
 var time = prompt("Enter the date in the format YYYY-MM-DD");
 
 var appData = {
-    budget : money,
-    timeData : time,
-    expenses : {},
-    optionalExpenses : {},
-    income : [],
-    savings : false 
+  budget : money,
+  timeData : time,
+  expenses : {},
+  optionalExpenses : {},
+  income : [],
+  savings : false 
 };
 
-let expenditure1 = prompt("Enter the required expenditure item for this month");
-let costs1 = +prompt("how much does it costs?");
-appData.expenses[expenditure1] = costs1;
+let counter = 0;
+while(counter < 2) {
+  let expenditure = prompt("Enter the required expenditure item for this month");
+  let costs = +prompt("how much does it costs?");
 
-let expenditure2 = prompt("Enter the required expenditure item for this month");
-let costs2 = +prompt("how much does it costs?");
-appData.expenses[expenditure2] = costs2;
+  if(expenditure != "" && expenditure.length < 50 
+    && costs != "" && !isNaN(costs)) {
+      appData.expenses[expenditure] = costs;
+      console.log("Done");
+      counter++;
+  } else {
+    console.log("Continue");
+    continue; 
+  }        
+}
 
-alert("Your budget for a month - " + money/30);
+appData.budgetPerDay = money/30;
+alert("Your budget for a month - " + appData.budgetPerDay);
+
+if(appData.budgetPerDay < 100) {
+  console.log("Fuck! Are you still alive?" );
+} else if(appData.budgetPerDay > 100 && appData.budgetPerDay < 2000) {
+  console.log("Wow! You are cool bro!");
+} else if(appData.budgetPerDay > 2000) {
+  console.log("Godlevel! I want to have children from you!");
+} else {
+  console.log("something is wrong!");
+}
